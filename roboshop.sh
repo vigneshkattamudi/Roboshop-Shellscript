25c637163bacb90597c13ee7b21b68672696da33
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-20 16:57:35 +0100

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z0443285370E15R10QIN" 
 DOMAIN_NAME="dsops84.space"
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
+#for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0f63039a616382c25 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
