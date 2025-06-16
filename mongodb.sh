f09df909ef0ba8a818012c385f28d4011cfc15ef
vigneshkattamudi <vkattamudi88@gmail.com>
2025-06-16 20:12:38 +0100

    Roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -49,3 +49,122 @@ VALIDATE $? "changing to Ipv4"
 
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "restarting mongodb"
+
+echo "MongoDB Setup Completed Successfully."
+
+# #!/bin/bash
+
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# LOGS_FOLDER="/var/log/roboshop-logs"
+# SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+# mkdir -p $LOGS_FOLDER
+# echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# if [ $USERID -ne 0 ]; then
+#     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+#     exit 1
+# else
+#     echo "You are running with root access" | tee -a $LOG_FILE
+# fi
+
+# VALIDATE(){
+#     if [ $1 -eq 0 ]; then
+#         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+#     else
+#         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+#         exit 1
+#     fi
+# }
+
+# COMMANDS=(
+#     "cp mongo.repo /etc/yum.repos.d/mongo.repo"
+#     "dnf install mongodb-org -y"
+#     "systemctl enable mongod"
+#     "systemctl start mongod"
+#     "sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf"
+#     "systemctl restart mongod"
+# )
+
+# DESCRIPTIONS=(
+#     "copying Mongodb.repo"
+#     "installing Mongodb"
+#     "enabling Mongodb"
+#     "starting Mongodb"
+#     "changing to Ipv4"
+#     "restarting Mongodb"
+# )
+
+# for i in "${!COMMANDS[@]}"; do
+#     ${COMMANDS[i]} &>>$LOG_FILE
+#     VALIDATE $? "${DESCRIPTIONS[i]}"
+# done
+
+
+# #!/bin/bash
+
+# # Stop the script if any command fails
+# set -e
+
+# # --- Configuration ---
+# LOGS_FOLDER="/var/log/roboshop-logs"
+# # More robust way to get the script name without extension
+# SCRIPT_NAME=$(basename "$0" .sh)
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+# # --- Colors for Output ---
+# R="\e[31m"
+# G="\e[32m"
+# N="\e[0m"
+
+# # --- Setup ---
+# mkdir -p "$LOGS_FOLDER"
+# # Redirect all script output (stdout and stderr) to a log file and also show it on the terminal
+# exec &> >(tee -a "$LOG_FILE")
+
+# echo "Script started executing at: $(date)"
+
+# # --- Pre-run Checks ---
+# if [ "$(id -u)" -ne 0 ]; then
+#    echo -e "$R ERROR:: Please run this script with root access $N"
+#    exit 1
+# fi
+
+# # --- Core Function ---
+# # This function executes a command and validates its success.
+# # It takes the description as the first argument and the command as the rest.
+# VALIDATE() {
+#     DESCRIPTION=$1
+#     # The 'shift' command removes the first argument, so $@ now contains only the command to run.
+#     shift
+#     COMMAND="$@"
+    
+#     echo -n "$DESCRIPTION... "
+    
+#     # Run the command, suppressing its output from the console but it will still be logged due to 'exec'
+#     $COMMAND &>/dev/null
+    
+#     # Check the exit status and print the result
+#     if [ $? -eq 0 ]; then
+#         echo -e "$G SUCCESS $N"
+#     else
+#         echo -e "$R FAILURE $N"
+#         echo "Check the log file for details: $LOG_FILE"
+#         exit 1
+#     fi
+# }
+
+# # --- Main Logic ---
+# VALIDATE "Copying MongoDB repo file"      cp mongo.repo /etc/yum.repos.d/mongo.repo
+# VALIDATE "Installing MongoDB"             dnf install mongodb-org -y
+# VALIDATE "Enabling MongoDB service"       systemctl enable mongod
+# VALIDATE "Starting MongoDB service"       systemctl start mongod
+# VALIDATE "Updating MongoDB listen address" sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+# VALIDATE "Restarting MongoDB service"     systemctl restart mongod
+
+# echo "MongoDB Setup Completed Successfully."
--- roboshop.sh
+++ roboshop.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+# AMI_ID="ami-09c813fb71547fc4f"
+# SG_ID="sg-0f63039a616382c25" 
+# INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+# ZONE_ID="Z0443285370E15R10QIN" 
+# DOMAIN_NAME="dsops84.space"
+
+# #for instance in ${INSTANCES[@]}
+# for instance in $@
+# do
+#     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0f63039a616382c25 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+#     if [ $instance != "frontend" ]
+#     then
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+#         RECORD_NAME="$instance.$DOMAIN_NAME"
+#     else
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+#         RECORD_NAME="$DOMAIN_NAME"
+#     fi
+#     echo "$instance IP address: $IP"
+
+#     aws route53 change-resource-record-sets \
+#     --hosted-zone-id $ZONE_ID \
+#     --change-batch '
+#     {
+#         "Comment": "Creating or Updating a record set for cognito endpoint"
+#         ,"Changes": [{
+#         "Action"              : "UPSERT"
+#         ,"ResourceRecordSet"  : {
+#             "Name"              : "'$RECORD_NAME'"
+#             ,"Type"             : "A"
+#             ,"TTL"              : 1
+#             ,"ResourceRecords"  : [{
+#                 "Value"         : "'$IP'"
+#             }]
+#         }
+#         }]
+#     }'
+# done
+
+# #!/bin/bash
+
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0f63039a616382c25" 
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
@@ -7,34 +48,55 @@ ZONE_ID="Z0443285370E15R10QIN"
 DOMAIN_NAME="dsops84.space"
 
 #for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in "$@"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0f63039a616382c25 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend" ]
-    then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t2.micro \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" \
+        --query "Instances[0].InstanceId" \
+        --output text)
+
+    # Wait until the instance is running and has public IP
+    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+
+    # Fetch both private and public IPs
+    PRIVATE_IP=$(aws ec2 describe-instances \
+        --instance-ids "$INSTANCE_ID" \
+        --query "Reservations[0].Instances[0].PrivateIpAddress" \
+        --output text)
+
+    PUBLIC_IP=$(aws ec2 describe-instances \
+        --instance-ids "$INSTANCE_ID" \
+        --query "Reservations[0].Instances[0].PublicIpAddress" \
+        --output text)
+
+    if [ "$instance" != "frontend" ]; then
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
         RECORD_NAME="$DOMAIN_NAME"
     fi
-    echo "$instance IP address: $IP"
+
+    echo "$instance launched:"
+    echo "  Private IP: $PRIVATE_IP"
+    echo "  Public IP : $PUBLIC_IP"
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
+    --hosted-zone-id "$ZONE_ID" \
+    --change-batch "
     {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
+        \"Comment\": \"Creating or Updating a record set for $RECORD_NAME\",
+        \"Changes\": [{
+            \"Action\": \"UPSERT\",
+            \"ResourceRecordSet\": {
+                \"Name\": \"$RECORD_NAME\",
+                \"Type\": \"A\",
+                \"TTL\": 1,
+                \"ResourceRecords\": [{
+                    \"Value\": \"$PRIVATE_IP\"
+                }]
+            }
         }]
-    }'
+    }"
 done
